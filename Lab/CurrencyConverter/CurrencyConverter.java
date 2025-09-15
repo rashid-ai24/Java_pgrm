c976793f721217bbea621f37795d5607d311479d
MOHD_RASHID <moh.rashid20006@gmail.com>
2025-09-15 20:51:44 +0530

    Add CurrencyConverter class with conversion methods

---
+++ Lab/CurrencyConverter/CurrencyConverter.java
@@ -0,0 +1,25 @@
+// 2. Currency, Distance, and Time Converter
+// File: CurrencyConverter.java
+package CurrencyConverter;
+
+public class CurrencyConverter {
+
+    public double dollortoinr(double x) {
+        return x * 67.86;
+    }
+    public double inrtodollor(double x) {
+        return x / 67.86;
+    }
+    public double eurotoinr(double x) {
+        return x * 79.18;
+    }
+    public double inrtoeuro(double x) {
+        return x / 79.18;
+    }
+    public double yentoinr(double x) {
+        return x * 0.62;
+    }
+    public double inrtoyen(double x) {
+        return x / 0.62;
+    }
+}
